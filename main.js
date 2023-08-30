1ae7018ab516e164aef4b1bca9439bbe657d4505
jeremiasV26 <jeremiasvaldez4@gmail.com>
2023-08-30 02:23:49 -0300

    ejercicio

---
+++ main.js
@@ -0,0 +1,54 @@
+// 1- Crear una función que reciba un número por parámetro e indique en consola si el número es par o impar.
+// function verificarParImpar(numero) {
+//   if (numero % 2 === 0) {
+//     console.log(numero + " es un número par.");
+//   } else {
+//     console.log(numero + " es un número impar.");
+//   }
+// }
+// verificarParImpar(8);
+
+// 2- Crear una función que reciba dos números por parámetro e indique en consola que número es mayor, y si ninguno lo es, indicar por consola que son iguales.
+
+// function mayor(numero1, numero2) {
+//   if (numero1 > numero2) {
+//     console.log(numero1 + " es mayor");
+//   } else if (numero2 > numero1) {
+//     console.log(numero2 + " es mayor");
+//   } else {
+//     console.log(" los numeros son iguales");
+//   }
+// }
+// mayor(8, 8);
+
+// 3- Crear una función que reciba un número por parámetro e indique en consola si ese número es múltiplo de 5.
+
+// function multiplo(numero1) {
+//   if (numero1 % 5 == 0) {
+//     console.log(numero1 + " es multiplo de 5");
+//   } else {
+//     console.log(numero1 + " no es multiplo de 5");
+//   }
+// }
+// multiplo(1);
+
+// 4- Crear una función que reciba un número por parámetro e imprima por consola todos los números desde el 0 hasta llegar a ese número.
+
+// function conteo(numero) {
+//   for (let i = 0; i <= numero; i++) {
+//     console.log(i);
+//   }
+// }
+// conteo(2);
+
+// 5 - Crear una función que reciba una palabra y un número por parámetro e imprima por consola  esa palabra la cantidad correspondiente al número indicado.
+// function repe(palabra, cantidad) {
+//   const repe = palabra.repeat(cantidad);
+//   console.log(repe);
+// }
+// repe(" Hola", 10);
+
+// 6 - Crear una función que reciba un array por parámetro e imprima por consola todos los valores de ese array.
+
+// 7 - Crear una función que reciba un array con 10 números e imprima por consola todos los valores de ese array, menos el que se encuentre en la 5ta posición del mismo. Ayuda: Recuerden que el primer índice de un array es "0".
+// 8 - Crea una función que reciba un array de números y un número por parámetro e imprima por consola cada número del array multiplicado por el número pasado por parámetro.
